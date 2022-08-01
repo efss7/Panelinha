0842db4aafd564daed61ca1c4c27dcfa099d42b6
Eric Felipe <ericfelipess07@gmail.com>
2022-08-01 15:48:08 -0300

    feat:Adicionado Light Theme

--- panelinha/src/routes/index.tsx
+++ panelinha/src/routes/index.tsx
@@ -1,9 +1,9 @@
 import { Navigate, Route, Routes } from "react-router-dom";
-
+import {Button} from "@mui/material"
 export const AppRoutes = () => {
     return(
         <Routes>
-            <Route path="/pagina-inicial" element={<p>Página inicial</p>}/>
+            <Route path="/pagina-inicial" element={<Button variant='contained' color="primary">Página inicial</Button>}/>
             
 
             <Route path="*" element={<Navigate to="/pagina-inicial"/>} />
