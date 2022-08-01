fbdc3e9c1634945b831c4e8932b822eaf971ccf7
Eric Felipe <ericfelipess07@gmail.com>
2022-08-01 15:18:15 -0300

    feat: Adicionado React-Router-Dom

--- panelinha/src/App.tsx
+++ panelinha/src/App.tsx
@@ -1,7 +1,10 @@
+import { BrowserRouter } from "react-router-dom";
+import { AppRoutes } from "./routes";
 
 export const App = () => {
   return (
-     <p>hello my friend</p>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   );
-}
-
+};
